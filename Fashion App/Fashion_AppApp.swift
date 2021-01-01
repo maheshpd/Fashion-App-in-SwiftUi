d485f5470c67ddab1b6f005e8bf54b90711c9d96
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-01 20:58:37 +0530

    Initial Commit

---
+++ Fashion App/Fashion_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fashion_AppApp.swift
+//  Fashion App
+//
+//  Created by Mahesh Prasad on 01/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Fashion_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
